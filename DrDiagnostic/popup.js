6e0ae9b2b26ed43c99a25baa2884a9fefc5e15aa
R.Malik <r.malik@criteo.com>
2022-04-12 10:51:11 +0100

    Code clean up, convert back from diagnostic mode

--- DrDiagnostic/popup.js
+++ DrDiagnostic/popup.js
@@ -1,24 +1,24 @@
-// Initialize button with users' preferred color
-let changeColor = document.getElementById("changeColor");
+// Initialize buttons
+let convertTo = document.getElementById("convertTo");
+let convertBack = document.getElementById("convertBack");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  convertTo.style.backgroundColor = color;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+// When the button is clicked, inject Convert to new page
+convertTo.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: convertToDiagnostic,
   });
 });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
-
+function convertToDiagnostic() {
 let oldURL = "https://d.eu.criteo.com/";
 let newURL = "http://cbsd.par.prod.crto.in/";
 let diagnosticMode = "&diagnosticmode=true"
@@ -26,6 +26,30 @@ let diagnosticMode = "&diagnosticmode=true"
   console.log(url);
   if(url.includes(oldURL)){
     url = url.replace(oldURL, newURL) + diagnosticMode;
+    window.open(url, '_blank').focus();
+  }
+}
+
+// When the button is clicked, inject Convert to old page
+convertBack.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: convertFromDiagnostic,
+  });
+});
+
+// The body of this function will be executed Convert to old page
+function convertFromDiagnostic() {
+let newURL = "https://d.eu.criteo.com/";
+let oldURL = "http://cbsd.par.prod.crto.in/";
+let diagnosticMode = "&diagnosticmode=true"
+  let url = window.location.href;
+  console.log(url);
+  if(url.includes(oldURL)){
+    url = url.replace(oldURL, newURL);
+    url = url.replace(diagnosticMode, "");
     window.open(url, '_blank').focus();
   }
 }
